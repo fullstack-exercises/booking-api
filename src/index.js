31ae1aa7f6ee09950ede9ba529ef25c30b802089
Jessica Michel <jessicamichel93@gmail.com>
2024-02-21 12:36:02 +0100

    Add login endpoint and packages

--- src/index.js
+++ src/index.js
@@ -1,10 +1,47 @@
 import express from "express";
+import loginRouter from "./routes/login.js";
+
+// Sentry
+import * as Sentry from "@sentry/node";
+import { ProfilingIntegration } from "@sentry/profiling-node";
 
 const app = express();
+
+Sentry.init({
+  dsn: "https://72545ff7a8ba3288a8886d86a06cb8b6@o4506665413640193.ingest.sentry.io/4506665424715776",
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Sentry.Integrations.Express({ app }),
+    new ProfilingIntegration(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set sampling rate for profiling - this is relative to tracesSampleRate
+  profilesSampleRate: 1.0,
+});
+
+// The request handler must be the first middleware on the app
+app.use(Sentry.Handlers.requestHandler());
+
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler());
+
+app.use(express.json());
+
+app.use("/login", loginRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello world!");
 });
+
+app.get("/debug-sentry", function mainHandler(req, res) {
+  throw new Error("My first Sentry error!");
+});
+
+// The error handler must be registered before any other error middleware and after all controllers
+app.use(Sentry.Handlers.errorHandler());
 
 app.listen(3000, () => {
   console.log("Server is listening on port 3000");
